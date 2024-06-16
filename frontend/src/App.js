12de31874012f71ddbc03bc724bc46be37038469
fwatanab <fwatanab@student.42tokyo.jp>
2024-06-17 02:50:13 +0900

    add router / ログイン画面を追加

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,19 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./Login";
 import ChatWindow from "./ChatWindow";
 import './App.css';
 
 function App() {
 	return (
-	<div className="App">
-		<ChatWindow />
-	</div>
+		<Router>
+			<div className="App">
+				<Routes>
+					<Route path="/" element={<Login />} />
+					<Route path="/chat" element={<ChatWindow />} />
+				</Routes>
+			</div>
+		</Router>
 	);
 }
 
