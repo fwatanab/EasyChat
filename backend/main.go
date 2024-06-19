c6b52e1f8d39291d340a9961ee5b14f4bf3d7efd
fwatanab <fwatanab@student.42tokyo.jp>
2024-06-19 18:49:42 +0900

    1日後にメッセージがデータベースから削除されるように修正

--- backend/main.go
+++ backend/main.go
@@ -5,10 +5,26 @@ import (
 	"easychat/handlers"
 	"log"
 	"net/http"
+	"time"
 )
 
 func main() {
+	// // データベース接続を初期化
 	database.InitDB("root:Ftsg0601_348441@tcp(127.0.0.1:3306)/")
+
+	// 定期的にメンテナンス処理を実行
+	go func() {
+		for {
+			// // 指定した日数経過したメッセージを削除
+			err := database.DeleteOldMessages(1)
+			if err != nil {
+				log.Printf("Failed to delete old messages: %v", err)
+			}
+			// 1日ごとに実行
+			time.Sleep(24 * time.Hour)
+		}
+	}()
+
 	// '/ws'エンドポイントへのリクエストを'handleConnections'で処理
 	http.HandleFunc("/ws", handlers.HandleConnections)
 	// 別のゴルーチンで'handleMessages'を実行
