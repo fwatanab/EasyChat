28faf9bae0aba633a415cbf2539b179f3ae63306
fwatanab <fwatanab@student.42tokyo.jp>
2024-06-19 18:53:51 +0900

    タイムスタンプxx:xxで表記, 1日以降はx日で表記

--- frontend/src/Message.js
+++ frontend/src/Message.js
@@ -4,10 +4,29 @@ const Message = ({ message }) => {
 	if (!message) {
 		return null;
 	}
+
+	function formatTimestamp(timestamp) {
+	const currentDate = new Date();
+	const messageDate = new Date(timestamp);
+
+	const timeDiff = currentDate - messageDate;
+	const hoursDiff = Math.floor(timeDiff / (1000 * 60 * 60));
+
+	if (hoursDiff < 24) {
+		const hours = messageDate.getHours().toString().padStart(2, "0");
+		const minutes = messageDate.getMinutes().toString().padStart(2, "0");
+		return `${hours}:${minutes}`;
+	} else {
+		const daysDiff = Math.floor(hoursDiff / 24);
+		return `${daysDiff} day(s)`;
+	}
+}
+
 	return (
 		<div className="message">
 			<li>
 				<strong>{message.name}: </strong>{message.inputMessage}
+				<span className="timestamp">{formatTimestamp(message.timestamp)}</span>
 			</li>
 		</div>
 	);
